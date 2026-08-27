57e030f3610bbe66c3aa9d5b47042a1c5ea94a95
10Draken01 <leonardofavion2@gmail.com>
2026-08-26 21:28:06 -0600

    Improve with Rate Limit

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import type { Express } from 'express';
 import { InicializateEndPoints } from './infrastructure/dependency-injection/container.js';
 import { errorHandler } from './infrastructure/middlewares/errorHandler.middleware.js';
+import { globalRateLimiter } from './infrastructure/middlewares/rateLimiter.middleware.js';
 
 export function createApp(dbPath: string): Express {
   const app = express();
   app.use(express.json());
+  app.use(globalRateLimiter);
+
   InicializateEndPoints(app, dbPath);
   app.use(errorHandler);
   return app;
